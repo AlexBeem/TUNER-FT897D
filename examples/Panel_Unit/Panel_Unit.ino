ef76c063d3f97bb9954f81ff8f704021693961ae
AlexBeem <71223577+AlexBeem@users.noreply.github.com>
2020-10-25 20:37:44 +0300

    Create Panel_Unit.ino

---
+++ examples/Panel_Unit/Panel_Unit.ino
@@ -0,0 +1,42 @@
+#include "ALSerial.h"
+#include "uniFT897D.h"
+#include <CustomSoftwareSerial.h>
+
+
+#define SOFTSERIAL  // если определено используем SofwareSerial, 
+                    // НАЗВАНИЕ не менять, используется в библиотеке
+//#define SERIAL1   //328PB ONLY
+
+#ifdef  SOFTSERIAL
+ALSerial RadioPort(2, 3); // RX, TX
+uniFT897D Radio(RadioPort);
+#else
+#ifdef SERIAL1
+ALSerial RadioPort(&Serial1);
+uniFT897D Radio(RadioPort);
+#else
+ALSerial RadioPort(&Serial);
+uniFT897D Radio(RadioPort);
+#endif
+#endif
+
+float ftFreq;
+uint32_t ssbFreq;
+
+
+void setup() {
+#ifdef SOFTSERIAL
+  Serial.begin(115200);
+  delay(250);
+  Serial.println("Start...");
+  Radio.Init(4800,CSERIAL_8N2);
+//  Radio.Init();  // Если инициализируем параметрами по умолчанию
+#else
+  Radio.Init(4800,SERIAL_8N2);
+//  Radio.Init();    // Если инициализируем параметрами по умолчанию
+#endif  
+}
+
+void loop() {
+
+}
